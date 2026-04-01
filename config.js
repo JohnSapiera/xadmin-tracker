c157d599d700ec83caecddc2d40470082c43733f
JohnSapiera <johnsapiera@gmail.com>
2026-04-01 23:23:19 +0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-export const weaponMap = {
-    "SIG-3A14D65A": "REDMI NOTE 14 PRO 5G",
-    "SIG-5B0D3A5C": "REALME 8 PRO"
-};
-
-export const getWeaponName = (sigID) => {
-    return weaponMap[sigID] || sigID; // Ibalik ang ID kung wala sa listahan
-};

bd98cb797658b66df90794193b5628636872ce73
JohnSapiera <johnsapiera@gmail.com>
2026-04-01 23:13:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 // config.js
 export const weaponMap = {
     "SIG-3A14D65A": "REDMI NOTE 14 PRO 5G",
-    "SIG-5B0D3A5C": "REALME 8 PRO",
-    "SIG-K882190": "XIAOMI REDMI NOTE 13",
-    "SIG-P001234": "POCO X6 PRO",
-    "SIG-V009988": "VIVO V30"
+    "SIG-5B0D3A5C": "REALME 8 PRO"
 };
 
 export const getWeaponName = (sigID) => {

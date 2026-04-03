40babf6896840ac9eccf3dda3f5218ab6f7f8ed0
JohnSapiera <johnsapiera@gmail.com>
2026-04-03 23:17:15 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js - Master Device Registry
+export const DEVICE_REGISTRY = {
+    "SIG-20452634": "REDMI NOTE 14 PRO",
+    "SIG-5B0D3A5C": "REALME 8 PRO",
+    "SIG-121B8F43": "Terchno Camon 40 PRO 5G"
+    
+};

bcc938e36f87b2645324b0a541ffaad3fee68415
JohnSapiera <johnsapiera@gmail.com>
2026-04-04 20:19:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 export const DEVICE_REGISTRY = {
     "SIG-20452634": "REDMI NOTE 14 PRO",
     "SIG-5B0D3A5C": "REALME 8 PRO",
-    "SIG-121B8F43": "Techno Camon 40 PRO 5G"
+    "SIG-121B8F43": "Techno Camon 40 PRO 5G",
+    "SIG-76CE9C42": "REDMI NOTE 12"
     
 };

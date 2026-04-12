d1ac3841b4dbbaa578417230c7ec497d8807ab23
JohnSapiera <johnsapiera@gmail.com>
2026-04-13 05:41:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,20 @@ export const DEVICE_REGISTRY = {
     "SIG-5B0D3A5C": "REALME 8 PRO",
     "SIG-121B8F43": "Techno Camon 40 PRO 5G",
     "SIG-76CE9C42": "REDMI NOTE 12"
-    
 };
+
+// Isama mo na rin dito ang Firebase DB connection para isang import na lang lahat
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = { 
+    apiKey: "AIzaSyD7SFXKTIx3ocIBD9B5JfWiI_sJmZPpbAI", 
+    authDomain: "my-admin-portal-12691.firebaseapp.com", 
+    projectId: "my-admin-portal-12691", 
+    storageBucket: "my-admin-portal-12691.firebasestorage.app", 
+    messagingSenderId: "317015091563", 
+    appId: "1:317015091563:web:baab5171d8e0a58acd442e" 
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);

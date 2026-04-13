02875cdef52320232931e8a2ae765b481c835078
JohnSapiera <johnsapiera@gmail.com>
2026-04-13 12:07:40 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-// script.js - CIA Profiles Main Logic
+// js/script.js - CIA Profiles Main Logic
+// CORRECT IMPORT PATH - from same folder (js/)
 import { db, DEVICE_REGISTRY, INTEL_TERMS } from "./config.js";
 import {
   collection,

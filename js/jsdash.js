26a02e91a33fcbc322500c150198c29fbafdd8f5
JohnSapiera <johnsapiera@gmail.com>
2026-04-14 14:00:39 +0800

    Update jsdash.js

--- js/jsdash.js
+++ js/jsdash.js
@@ -1,6 +1,6 @@
 // js/jsdash.js - CORE DASHBOARD WITH SOUNDS (COMPLETE)
 
-import SoundFX from './sound.js';
+import SoundFX from '../sound.js';
 import { DEVICE_REGISTRY } from '../config.js';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { 

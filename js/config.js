bd54853aae5b6f2588f30707c67cc01025b96393
JohnSapiera <johnsapiera@gmail.com>
2026-04-15 10:07:03 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 // Device Registry - Maps device IDs to readable names
 export const DEVICE_REGISTRY = {
   "SIG-20452634": "REDMI NOTE 14 PRO",
-  "SIG-3A14D65A": "REALME 8 PRO",
+  "SIG-5B0D3A5C": "REALME 8 PRO",
   "SIG-121B8F43": "Techno Camon 40 PRO 5G",
   "SIG-76CE9C42": "REDMI NOTE 12"
 };
